57b4f8fbcc7de8854cbf9602038cd9ddcfac4815
Jamessom Queiroz <jamessomqueiroz@gmail.com>
2019-05-29 15:38:23 -0300

    base project

---
+++ bin/main.dart
@@ -0,0 +1,9 @@
+// import 'package:hello/hello.dart' as hello;
+
+// main(List<String> arguments) {
+//   print('Hello world: ${hello.calculate()}!');
+// }
+
+void main() {
+  print('Dart console sample');
+}
